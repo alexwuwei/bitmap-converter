502086e643f5b493cd510bbefd3a15d6d6ef36a1
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-05 10:33:15 -0800

    initialized test file

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var expect = require('chai').expect;
+var program = require('../lib/');
+
+describe('sometest' function() {
+  it('should do something', function(done){
+    process.nextTick(function(){
+      expect().to.equal();
+      done()
+    });
+  });
+});
