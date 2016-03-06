e0856fb23ab3db8b3e66cb3d8d48ea3676cfaf6e
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-05 17:51:00 -0800

    initialized color conversion functions, currently testing output

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@
 var expect = require('chai').expect;
 var program = require('../lib/');
 
-describe('sometest' function() {
-  it('should do something', function(done){
+describe('sometest', function() {
+  it('should find a header', function(done){
     process.nextTick(function(){
-      expect().to.equal();
+      expect(bitmapData.size).to.equal(11078);
       done()
     });
   });
