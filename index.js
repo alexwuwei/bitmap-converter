69ff98b6f79fc7816ee54c722ca1908e322ef3b0
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-07 09:20:28 -0800

    completed palette and non palette functionality, completed grayscale and bluescale logic for palette and non palette

--- index.js
+++ index.js
@@ -10,15 +10,73 @@ console.log('OS endianness is: ' + leOrBe);
 var bitmapImage = fs.readFileSync(__dirname + '/img/' + process.argv[2]);
 var bitmapHeadData = processHeader(bitmapImage);
 
+var argument = process.argv[3];
+var bitmapName = process.argv[2];
 
-for (var i = bitmapHeadData.pixelArrayStartLocation; i < bitmapHeadData.size - 4; i += 4) {
+var transform;
+
+if (argument === 'invert' && bitmapName === 'non-palette-bitmap.bmp') {
+  transform = inverted;
+} else if (argument === 'grayscale' && bitmapName === 'non-palette-bitmap.bmp') {
+  transform = grayscale;
+} else if (argument === 'bluescale' && bitmapName === 'non-palette-bitmap.bmp') {
+  transform = bluescale;
+} else if (argument === 'invert' && bitmapName === 'palette-bitmap.bmp') {
+  transform = invertedPalette;
+} else if (argument === 'grayscale' && bitmapName === 'palette-bitmap.bmp') {
+  transform = grayscalePalette;
+} else if (argument === 'bluescale' && bitmapName === 'palette-bitmap.bmp') {
+  transform = bluescalePalette;
+}
+
+function inverted () {
+  for (var i = bitmapHeadData.pixelArrayStartLocation; i < bitmapHeadData.size - 4; i += 4) {
   var invertedObject = colorHandler.invertColors(colorHandler.readColors(i, bitmapImage));
   colorHandler.writeColors(i, bitmapImage, invertedObject);
   colorHandler.readColors(i, bitmapImage);
+};
 }
 
-// var writeFiles = function(bitmap) {
-//   fs.writeFileSync(__dirname + '/img-output/' + process.argv[2], bitmap);
-// }
+function grayscale () {
+  for (var i = bitmapHeadData.pixelArrayStartLocation; i < bitmapHeadData.size - 4; i += 4) {
+  var invertedObject = colorHandler.grayscaleColors(colorHandler.readColors(i, bitmapImage));
+  colorHandler.writeColors(i, bitmapImage, invertedObject);
+  colorHandler.readColors(i, bitmapImage);
+};
+}
+
+function bluescale () {
+  for (var i = bitmapHeadData.pixelArrayStartLocation; i < bitmapHeadData.size - 4; i += 4) {
+  var invertedObject = colorHandler.bluescaleColors(colorHandler.readColors(i, bitmapImage));
+  colorHandler.writeColors(i, bitmapImage, invertedObject);
+  colorHandler.readColors(i, bitmapImage);
+};
+}
+
+function invertedPalette () {
+  for (var i = 54; i < bitmapHeadData.pixelArrayStartLocation; i += 4) {
+  var invertedObject = colorHandler.invertColors(colorHandler.readColors(i, bitmapImage));
+  colorHandler.writeColors(i, bitmapImage, invertedObject);
+  colorHandler.readColors(i, bitmapImage);
+};
+}
+
+function grayscalePalette () {
+  for (var i = 54; i < bitmapHeadData.pixelArrayStartLocation; i += 4) {
+  var invertedObject = colorHandler.grayscaleColors(colorHandler.readColors(i, bitmapImage));
+  colorHandler.writeColors(i, bitmapImage, invertedObject);
+  colorHandler.readColors(i, bitmapImage);
+};
+}
+
+function bluescalePalette () {
+  for (var i = 54; i < bitmapHeadData.pixelArrayStartLocation; i += 4) {
+  var invertedObject = colorHandler.bluescaleColors(colorHandler.readColors(i, bitmapImage));
+  colorHandler.writeColors(i, bitmapImage, invertedObject);
+  colorHandler.readColors(i, bitmapImage);
+};
+}
+
+transform();
 
 fs.writeFileSync(__dirname + '/img-output/' + process.argv[2], bitmapImage);
