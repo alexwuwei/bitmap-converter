46776ffaead15d96e97dffbc4f863009b42a2ab7
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-07 09:52:58 -0800

    moved lint parameters to .eslintrc, added color tests, fixed header data read tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,23 @@
 'use strict';
 
 var gulp = require('gulp'),
-    lint = require('gulp-eslint'),
-    mocha = require('gulp-mocha'),
-    chai = require('gulp-chai'),
-    paths = ['*.js', 'lib/*.js', 'test/*.js', 'index.js', 'gulpfile.js'];
+  lint = require('gulp-eslint'),
+  mocha = require('gulp-mocha'),
+  chai = require('gulp-chai'),
+  paths = ['*.js', 'lib/*.js', 'test/*.js', 'index.js', 'gulpfile.js'];
 
-gulp.task('lint' function(){
-  return gulp.src(paths);
-    .pipe(lint({
-  "rules": {
-    "no-console": 0,
-    "indent": [
-      2,
-      2
-    ],
-    "quotes": [
-      2,
-      "single"
-    ],
-    "linebreak-style": [
-      2,
-      "unix"
-        ],
-        "semi": [
-          2,
-          "always"
-        ]
-  },
-  "env": {
-    "es6": true,
-    "node": true,
-    "browser": true
-  },
-  "globals": {
-    "describe": false,
-    "it": false,
-    "beforeEach": false,
-    "afterEach": false,
-    "before": false,
-    "after": false
-  },
-  "ecmaFeatures": {
-    "modules": true,
-    "experimentalObjectRestSpread": true
-  },
-  "extends": "eslint:recommended"
-}
-))
+gulp.task('eslint', function(){
+  return gulp.src(paths)
+    .pipe(lint())
   .pipe(lint.formatEach('compact', process.stderr));
 });
 
-gulp.task('mocha' function(){
+gulp.task('mocha', function(){
   return gulp.src(paths, {read: false})
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('watch' function(){
+gulp.task('watch', function(){
   gulp.watch(paths, ['mocha', 'eslint'])
 });
 
